7a3f5466db446d48e7405e30b8a8166043bfb8ca
rezzza07 <notfunnyatall0007@gmail.com>
2025-12-18 13:46:31 +0600

    Courier GET using ID

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ async function run() {
 
         })
 
+        app.get('/courier/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await courierCollection.findOne(query);
+            res.send(result);
+        })
+
         // Courier Delete
         app.delete('/courier/:id', async (req, res) => {
             const id = req.params.id;
